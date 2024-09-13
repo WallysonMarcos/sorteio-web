de6d4761d758a247e7206aa6b799c9b841596fbb
Wallyson Silva <wallyson78@gmail.com>
2024-09-13 13:42:25 -0400

    update basePath

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/sorteio-web-github-pages",
+    basePath: "/sorteio-web",
 
     /**
      * Disable server-based image optimization. Next.js does not support
